c8481b701681ac2c75e7fd570f1853f8bbf3f444
epub <yoaevelyn@gmail.com>
2022-03-16 00:33:42 -0400

    get monsters href

--- src/mhr_scraping/core.clj
+++ src/mhr_scraping/core.clj
@@ -3,6 +3,7 @@
             [hickory.select :as hs]
             [hickory.core :as hc]))
 
+
 (defn -main
   []
   (let [response (client/get "https://mhrise.kiranico.com/data/monsters")]
@@ -13,12 +14,16 @@
            (hs/select (hs/descendant (hs/tag :aside)
                                      (hs/tag :li)
                                      (hs/tag :a)))
-           (map (fn [element]
-                  element
-                  #_(->> (hs/select (hs/tag :h3)))
-                  #_(let img 
-                    (-> (hs/select (hs/tag :img) element)
-                        (first)
-                        (get-in [:attrs :src]))))
-                 )))))
-
+           (map (fn [{:keys [attrs] :as element}]
+                  (let [href (get attrs :href)
+                        name 
+                        (-> (hs/select (hs/tag :h3) element)
+                            (first)
+                            (get :content))
+                        
+                        img
+                        (-> (hs/select (hs/tag :img) element)
+                            (first)
+                            (get-in [:attrs :src]))]
+                    {:name name :img img :href href})
+                  ))))))
