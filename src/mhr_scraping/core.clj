1f1b3d6182433d6b4d09399e013f10cf28e68ca1
epub <yoaevelyn@gmail.com>
2022-03-15 23:23:53 -0400

    add monsters scraping

--- src/mhr_scraping/core.clj
+++ src/mhr_scraping/core.clj
@@ -7,9 +7,18 @@
   []
   (let [response (client/get "https://mhrise.kiranico.com/data/monsters")]
     (when (= (:status response) 200)
-      (:body response)
-      (def parsed-html (-> (:body response) hc/parse hc/as-hickory))
-      (-> (hs/select 
-           (hs/child (hs/tag :aside)
-                     (hs/descendant (hs/tag :ul)))  
-           parsed-html)))))
+      (->> (:body response) 
+           hc/parse 
+           hc/as-hickory
+           (hs/select (hs/descendant (hs/tag :aside)
+                                     (hs/tag :li)
+                                     (hs/tag :a)))
+           (map (fn [element]
+                  element
+                  #_(->> (hs/select (hs/tag :h3)))
+                  #_(let img 
+                    (-> (hs/select (hs/tag :img) element)
+                        (first)
+                        (get-in [:attrs :src]))))
+                 )))))
+
